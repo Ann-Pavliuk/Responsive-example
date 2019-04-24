20a37b37b08b587de02e65b907ae7a6c0c058f21
Ann-Pavliuk <43817902+Ann-Pavliuk@users.noreply.github.com>
2019-04-24 13:45:39 +0300

    Add files

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.konvert').on('click', function(e){
+	$('.konv_top').toggleClass('konv_top_close');
+	 //$('.konvert').css({"transform": "rotateY(180deg)"});
+	 //$('konvert_back').css({"transform": "rotateY(360deg)"});
+	$('.paper').toggleClass('paper_close');
+});
